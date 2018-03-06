e04b426c33e2ddb52e435800a0b94a437b502950
Ryan Desmond <radesmond@gmail.com>
2018-03-06 12:04:32 +0800

    tetsing upstream

--- part_01/Exercise_01.java
+++ part_01/Exercise_01.java
@@ -4,7 +4,7 @@ package part_01;
  * Part 1 Exercise 1:
  *      Write the necessary code to display the follow message to the console
  *
- *      Hello World!
+ *      Hello World!!
  *      I'm a programmer now.
  *      Yeehaw!
  */
